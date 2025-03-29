1dd97bb4886a6dc436ef5797b398e5e43a5289c1
Ronald <ronald.jian@gmail.com>
2025-03-29 18:19:53 +0800

    add permission model

--- config/permission.php
+++ config/permission.php
@@ -13,7 +13,7 @@ return [
          * `Spatie\Permission\Contracts\Permission` contract.
          */
 
-        'permission' => Spatie\Permission\Models\Permission::class,
+        'permission' => \Sharenjoy\NoahCms\Models\Permission::class,
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which
---
+++ src/Models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sharenjoy\NoahCms\Models;
+
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    //
+}
